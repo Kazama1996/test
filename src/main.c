268db416fefce70f726bfd8e97ccc6ac7db6f0e5
Yuta Kazama <Y.Kazama1996@gmail.com>
2022-11-28 21:03:44 +0800

    modified feature A
    create A1~4

--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 int main()
 {
 	printf("Hello World!\n");
-	printf("this is featureA");
+	printf("this is modified feature A");
 	printf("this is feature B");
+	printf("a1");
+	printf("a2");
+	printf("a3");
+	printf("a4");
 	return (0);
 }
